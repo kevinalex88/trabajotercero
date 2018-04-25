382323f19615e28cb245d9780866588bd173e4f2
USUARIO <USUARIO@DESKTOP-6IV6I7P>
2018-04-24 20:20:08 -0500

    xd

--- src/trabajomenu/ordenamiento.java
+++ src/trabajomenu/ordenamiento.java
@@ -8,12 +8,24 @@ public class ordenamiento {
         
       estudiante[] arrayEstudiantes = new estudiante[5];
       arrayEstudiantes[0] = new estudiante("juan", "polania","123", 5, 5.0);
-      arrayEstudiantes[1] = new estudiante("juan", "polania","123", 4, 4.5);
-      arrayEstudiantes[2] = new estudiante("juan", "polania","123", 3, 4.8);
-      arrayEstudiantes[3] = new estudiante("juan", "polania","123", 2, 4.2);
-      arrayEstudiantes[4] = new estudiante("juan", "polania","123", 1, 2.0);
-      arrayEstudiantes[5] = new estudiante("juan", "polania","123", 6, 3.5);
+      arrayEstudiantes[1] = new estudiante("kevin", "bermeo","123", 4, 4.5);
+      arrayEstudiantes[2] = new estudiante("andres", "garcia","123", 3, 4.8);
+      arrayEstudiantes[3] = new estudiante("pedro", "polania","123", 2, 4.2);
+      arrayEstudiantes[4] = new estudiante("camilo", "sanchez","123", 1, 2.0);
+      arrayEstudiantes[5] = new estudiante("juan", "pola","123", 6, 3.5);
+      
+      int temp;
+        for(int i=1;i<arrayEstudiantes.length;i++){
+            for(int j=0;j<arrayEstudiantes.length;j++){
+                if(arrayEstudiantes[j].getNota()<arrayEstudiantes[j+1].getNota()){
+                    temp=arrayEstudiantes[j].getPuesto();
+                    arrayEstudiantes[j].setPuesto(arrayEstudiantes[j+1].getPuesto());
+                    arrayEstudiantes[j+1].setPuesto(temp);
+                }
+            }
         
     }
+    
+}
     
 }
